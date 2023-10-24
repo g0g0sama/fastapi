5657c291b401a6616c35001e87b7a0ea41059c77
Göksucan Erkoç <goksucanerkoc@gmail.com>
2023-10-24 18:01:49 +0300

    sqlalchemy etc. added.

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from .database import Base
+from sqlalchemy.sql.expression import null, text
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable= False)
+    price = Column(Integer, nullable= False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable= False, server_default=text("now()"))
+
+
